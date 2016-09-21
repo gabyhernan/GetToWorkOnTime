dd584f6e257668d3ce6a1ef5cc8acfa9b93a19d1
Gaby Hernandez <gabyhernandezlebron@gmail.com>
2016-09-21 17:56:22 -0400

    before installing sessions

--- mta_prac/mta_prac/app/controllers/users_controller.rb
+++ mta_prac/mta_prac/app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
 
+   #attr_accessor :email, :password, :full_name, :phone_number, :train_lines, :commute_time, :schedule, :zipcode
+
 def index
+  #@user = User.find(params[:id])
 end
 
 def show
+  @user = User.find(params[:id])
   end
 
 # GET /users/new
@@ -13,15 +17,19 @@ end
 
 def create
   @user = User.new(user_params)
-  @user.password = params[:password]
-  @user.save!
-  redirect_to 'index'
+  #@user.password = params[:password]
+  if @user.save!
+    flash[:success] = "Welcome #{@user.full_name}!"
+    redirect_to 'index'
+  else
+    #render 'new'
+  end
 end
 
 private
 # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:full_name, :email, :password, :phone_number, :train_lines, :commute_time, :schedule, :zipcode)
+      params.require(:user).permit(:email, :password, :full_name, :phone_number, :train_lines, :commute_time, :schedule, :zipcode)
     end
 
 end
--- mta_prac/mta_prac/config/routes.rb
+++ mta_prac/mta_prac/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  root to: 'users#signup'
+  root to: 'users#signin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
 end
