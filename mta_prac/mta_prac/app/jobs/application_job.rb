cd5dce31c9245e6b4e973d59c83f528e48efbe5c
Gaby Hernandez <gabyhernandezlebron@gmail.com>
2016-09-26 15:16:18 -0400

    twilio set up and styling added

--- mta_prac/mta_prac/app/jobs/application_job.rb
+++ mta_prac/mta_prac/app/jobs/application_job.rb
@@ -1,50 +1,55 @@
-class ApplicationJob < ActiveJob::Base
-  queue_as :default
+# require'twilio-ruby'
+# class ApplicationJob < ActiveJob::Base
+#   queue_as :default
 
-  before_perform do #|variables| Might be needed
-    # invoke another job at your time of choice
-    ApplicationJob.set(wait: 60.seconds).perform_later
-  end
+#   before_perform do #|variables| Might be needed
+#     # invoke another job at your time of choice
+#     ApplicationJob.set(wait: 300.seconds).perform_later
+#   end
 
-  def perform(*args) #Might be needed
- response = HTTParty.get("http://mtastat.us/api/trains")
- full_sanitizer = Rails::Html::FullSanitizer.new
- no_tags  = full_sanitizer.sanitize(response)
+#   def perform(*args) #Might be needed
+#  response = HTTParty.get("http://mtastat.us/api/trains")
+#  full_sanitizer = Rails::Html::FullSanitizer.new
+#  no_tags  = full_sanitizer.sanitize(response)
 
-  body = JSON.parse(no_tags)
-  train_change = []
+#   body = JSON.parse(no_tags)
+#   train_change = []
 
-  trains = body.each do |line|
-   #name = puts line['name']
-   #status = puts line['status']
-    if line['status'] != "all good."
-      train_change.push(line)
-    end
-  end
+#   trains = body.each do |line|
+#    #name = puts line['name']
+#    #status = puts line['status']
+#     if line['status'] != "all good."
+#       train_change.push(line)
+#     end
+#   end
 
-  train_change.each do |line|
-    # time fixin
-    @user = User.where("train_lines LIKE ?  AND  #{Time.now.strftime("%A").downcase} != ''
-      AND strftime('%s',#{Time.now.strftime("%A").downcase}) < strftime('%s','09:05')"
-       ,'%'+line['name']+'%' ).all
+#   train_change.each do |line|
+#     # time fixin
+#     @user = User.where("train_lines LIKE ?  AND  #{Time.now.strftime("%A").downcase} != ''
+#       AND strftime('%s',#{Time.now.strftime("%A").downcase}) < strftime('%s','09:05')",'%'+line['name']+'%' ).all
 
-   @user.each do |sheep|
-    puts sheep['id'].to_s + sheep['phone_number'].to_s + line['name'] + line['status']
-    #call twilio in this loop
-  end
-end
+#    @user.each do |sheep|
+#     puts sheep['id'].to_s + " " + sheep['phone_number'].to_s + " " + line['name'] + " " + line['status']
+#     client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
+#     client.messages.create :to => sheep['phone_number'],
+#     :from => ENV["TWILIO_NUMBER"],
+#     :body => "Hey there" + " " + sheep['full_name']+"  just wanted to let you know there is a change on Line "+line['name']+ " it's " + line['status']
 
-  puts trains.count
-  puts train_change.count
-    #byebug #debugging
-   # Do something later
+#     #call twilio in this loop
+#   end
+# end
 
-   #@users = Users.all
-    #puts "Hello There "
-    #puts trains
+#   puts trains.count
+#   puts train_change.count
+#     #byebug #debugging
+#    # Do something later
 
-  end
-end
+#    #@users = Users.all
+#     #puts "Hello There "
+#     #puts trains
+
+#   end
+# end
 
 
 
